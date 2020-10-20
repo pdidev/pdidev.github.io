e0696c43fefe908929e825f8a0b9cd5a26cc9de6
jbigot <jbigot@noreply.example.com>
2020-10-20 15:19:30 +0000

    New version deployed by repository_dispatch 76

--- master/namespacePDI.js
+++ master/namespacePDI.js
@@ -1,20 +1,5 @@
 var namespacePDI =
 [
-    [ "New release", "autotoc_md4.html#autotoc_md5", null ],
-    [ "Dependency change", "autotoc_md4.html#autotoc_md6", null ],
-    [ "Setup", "Hands_on.html#autotoc_md7", null ],
-    [ "Tutorial", "Hands_on.html#autotoc_md8", [
-      [ "Ex1. Getting started", "Hands_on.html#autotoc_md9", null ],
-      [ "Ex2. Now with some PDI", "Hands_on.html#autotoc_md10", null ],
-      [ "Ex3. HDF5 through PDI", "Hands_on.html#autotoc_md11", null ],
-      [ "Ex4. Writing some real data", "Hands_on.html#autotoc_md12", null ],
-      [ "Ex5. Introducing events", "Hands_on.html#autotoc_md13", null ],
-      [ "Ex6. Simplifying the code", "Hands_on.html#autotoc_md14", null ],
-      [ "Ex7. Writing a selection", "Hands_on.html#autotoc_md15", null ],
-      [ "Ex8. Writing an advanced selection", "Hands_on.html#autotoc_md16", null ],
-      [ "Ex9. Going parallel", "Hands_on.html#autotoc_md17", null ]
-    ] ],
-    [ "What next ?", "Hands_on.html#autotoc_md18", null ],
     [ "Array_datatype", "classPDI_1_1Array__datatype.html", "classPDI_1_1Array__datatype" ],
     [ "Callbacks", "classPDI_1_1Callbacks.html", "classPDI_1_1Callbacks" ],
     [ "Config_error", "structPDI_1_1Config__error.html", "structPDI_1_1Config__error" ],
--- master/navtreedata.js
+++ master/navtreedata.js
@@ -4,7 +4,23 @@ var NAVTREE =
     [ "About", "index.html", "index" ],
     [ "C API Reference", "modules.html", "modules" ],
     [ "Checklists", "autotoc_md4.html", null ],
-    [ "Hands-on tutorial", "Hands_on.html", null ],
+    [ "Hands-on tutorial", "Hands_on.html", [
+      [ "Setup", "Hands_on.html#autotoc_md7", null ],
+      [ "Tutorial", "Hands_on.html#autotoc_md8", [
+        [ "New release", "autotoc_md4.html#autotoc_md5", null ],
+        [ "Dependency change", "autotoc_md4.html#autotoc_md6", null ],
+        [ "Ex1. Getting started", "Hands_on.html#autotoc_md9", null ],
+        [ "Ex2. Now with some PDI", "Hands_on.html#autotoc_md10", null ],
+        [ "Ex3. HDF5 through PDI", "Hands_on.html#autotoc_md11", null ],
+        [ "Ex4. Writing some real data", "Hands_on.html#autotoc_md12", null ],
+        [ "Ex5. Introducing events", "Hands_on.html#autotoc_md13", null ],
+        [ "Ex6. Simplifying the code", "Hands_on.html#autotoc_md14", null ],
+        [ "Ex7. Writing a selection", "Hands_on.html#autotoc_md15", null ],
+        [ "Ex8. Writing an advanced selection", "Hands_on.html#autotoc_md16", null ],
+        [ "Ex9. Going parallel", "Hands_on.html#autotoc_md17", null ]
+      ] ],
+      [ "What next ?", "Hands_on.html#autotoc_md18", null ]
+    ] ],
     [ "Deprecated List", "deprecated.html", null ],
     [ "Plugin API reference", "annotated.html", "annotated" ]
   ] ]
