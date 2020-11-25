a9ef6ef35ac6adad625862456c525910dea66b56
jbigot <jbigot@noreply.example.com>
2020-11-25 10:57:17 +0000

    New version deployed by push 101

---
+++ 0.6.1/navtreedata.js
@@ -0,0 +1,62 @@
+var NAVTREE =
+[
+  [ "PDI", "index.html", [
+    [ "About", "index.html", "index" ],
+    [ "C API Reference", "modules.html", "modules" ],
+    [ "Plugins", "Plugins.html", "Plugins" ],
+    [ "Specification tree Reference", "Specification_tree_ref.html", [
+      [ "specification tree root", "Specification_tree_ref.html#root_node", null ],
+      [ "array_type", "Specification_tree_ref.html#array_type_node", null ],
+      [ "char_type", "Specification_tree_ref.html#char_type_node", null ],
+      [ "character_type", "Specification_tree_ref.html#character_type_node", null ],
+      [ "datatype", "Specification_tree_ref.html#datatype_node", null ],
+      [ "data_map", "Specification_tree_ref.html#data_map_node", null ],
+      [ "double_type", "Specification_tree_ref.html#double_type_node", null ],
+      [ "$-expression", "Specification_tree_ref.html#expression_node", null ],
+      [ "float_type", "Specification_tree_ref.html#float_type_node", null ],
+      [ "int_type", "Specification_tree_ref.html#int_type_node", null ],
+      [ "int16_type", "Specification_tree_ref.html#int16_type_node", null ],
+      [ "int32_type", "Specification_tree_ref.html#int32_type_node", null ],
+      [ "int64_type", "Specification_tree_ref.html#int64_type_node", null ],
+      [ "int8_type", "Specification_tree_ref.html#int8_type_node", null ],
+      [ "integer_type", "Specification_tree_ref.html#integer_type_node", null ],
+      [ "intexpr_seq", "Specification_tree_ref.html#intexpr_seq_node", null ],
+      [ "intexpr_or_seq", "Specification_tree_ref.html#intexpr_or_seq_node", null ],
+      [ "logical_type", "Specification_tree_ref.html#logical_type_node", null ],
+      [ "member_desc", "Specification_tree_ref.html#member_desc_node", null ],
+      [ "members_map", "Specification_tree_ref.html#members_map_node", null ],
+      [ "plugin_map", "Specification_tree_ref.html#plugin_map_node", null ],
+      [ "real_type", "Specification_tree_ref.html#real_type_node", null ],
+      [ "record_type", "Specification_tree_ref.html#record_type_node", null ],
+      [ "simple_datatype", "Specification_tree_ref.html#simple_datatype_node", null ]
+    ] ],
+    [ "Hands-on", "Hands_on.html", [
+      [ "Environment", "Hands_on.html#autotoc_md14", [
+        [ "Poincare machine", "Hands_on.html#autotoc_md15", null ]
+      ] ],
+      [ "Setup", "Hands_on.html#autotoc_md16", null ],
+      [ "Tutorial", "Hands_on.html#autotoc_md17", [
+        [ "Ex1. Getting started", "Hands_on.html#autotoc_md18", null ],
+        [ "Ex2. Now with some PDI", "Hands_on.html#autotoc_md19", null ],
+        [ "Ex3. HDF5 through PDI", "Hands_on.html#autotoc_md20", null ],
+        [ "Ex4. Writing some real data", "Hands_on.html#autotoc_md21", null ],
+        [ "Ex5. Introducing events", "Hands_on.html#autotoc_md22", null ],
+        [ "Ex6. Simplifying the code", "Hands_on.html#autotoc_md23", null ],
+        [ "Ex7. writing a selection", "Hands_on.html#autotoc_md24", null ],
+        [ "Ex8. going parallel", "Hands_on.html#autotoc_md25", null ]
+      ] ],
+      [ "What next ?", "Hands_on.html#autotoc_md26", null ]
+    ] ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Plugin API reference", "annotated.html", "annotated" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classPDI_1_1Record__datatype.html#acf74cad00490ddb45a9bb36b6f30c3fd"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
