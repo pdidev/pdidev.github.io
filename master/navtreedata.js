873a2b8c9b70bd29b7cf6c4217b6b143c2ff9ac1
jbigot <jbigot@noreply.example.com>
2021-01-16 17:20:53 +0000

    New version deployed by repository_dispatch 110

--- master/navtreedata.js
+++ master/navtreedata.js
@@ -3,24 +3,25 @@ var NAVTREE =
   [ "PDI", "index.html", [
     [ "About", "index.html", "index" ],
     [ "C API Reference", "modules.html", "modules" ],
-    [ "Checklists", "autotoc_md4.html", null ],
+    [ "Checklists", "Checklists.html", [
+      [ "New patch release", "Checklists.html#autotoc_md4", null ],
+      [ "New minor or major release", "Checklists.html#autotoc_md5", null ],
+      [ "Dependency change", "Checklists.html#autotoc_md6", null ]
+    ] ],
     [ "Hands-on tutorial", "Hands_on.html", [
-      [ "Setup", "Hands_on.html#autotoc_md8", null ],
-      [ "Tutorial", "Hands_on.html#autotoc_md9", [
-        [ "New patch release", "autotoc_md4.html#autotoc_md5", null ],
-        [ "New minor or major release", "autotoc_md4.html#autotoc_md6", null ],
-        [ "Dependency change", "autotoc_md4.html#autotoc_md7", null ],
-        [ "Ex1. Getting started", "Hands_on.html#autotoc_md10", null ],
-        [ "Ex2. Now with some PDI", "Hands_on.html#autotoc_md11", null ],
-        [ "Ex3. HDF5 through PDI", "Hands_on.html#autotoc_md12", null ],
-        [ "Ex4. Writing some real data", "Hands_on.html#autotoc_md13", null ],
-        [ "Ex5. Introducing events", "Hands_on.html#autotoc_md14", null ],
-        [ "Ex6. Simplifying the code", "Hands_on.html#autotoc_md15", null ],
-        [ "Ex7. Writing a selection", "Hands_on.html#autotoc_md16", null ],
-        [ "Ex8. Writing an advanced selection", "Hands_on.html#autotoc_md17", null ],
-        [ "Ex9. Going parallel", "Hands_on.html#autotoc_md18", null ]
+      [ "Setup", "Hands_on.html#autotoc_md7", null ],
+      [ "Tutorial", "Hands_on.html#autotoc_md8", [
+        [ "Ex1. Getting started", "Hands_on.html#autotoc_md9", null ],
+        [ "Ex2. Now with some PDI", "Hands_on.html#autotoc_md10", null ],
+        [ "Ex3. HDF5 through PDI", "Hands_on.html#autotoc_md11", null ],
+        [ "Ex4. Writing some real data", "Hands_on.html#autotoc_md12", null ],
+        [ "Ex5. Introducing events", "Hands_on.html#autotoc_md13", null ],
+        [ "Ex6. Simplifying the code", "Hands_on.html#autotoc_md14", null ],
+        [ "Ex7. Writing a selection", "Hands_on.html#autotoc_md15", null ],
+        [ "Ex8. Writing an advanced selection", "Hands_on.html#autotoc_md16", null ],
+        [ "Ex9. Going parallel", "Hands_on.html#autotoc_md17", null ]
       ] ],
-      [ "What next ?", "Hands_on.html#autotoc_md19", null ]
+      [ "What next ?", "Hands_on.html#autotoc_md18", null ]
     ] ],
     [ "Deprecated List", "deprecated.html", null ],
     [ "Plugin API reference", "annotated.html", "annotated" ]
