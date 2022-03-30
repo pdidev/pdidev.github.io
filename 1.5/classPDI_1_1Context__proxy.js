5c4cb7e7890517008d7a623558b70f0a7d5c3d97
jbigot <jbigot@noreply.example.com>
2022-03-30 10:46:25 +0000

    Update to match 5f9d73aab95238f88965dc2c2b1e064b58f26fe5 by repository_dispatch 254

---
+++ 1.5/classPDI_1_1Context__proxy.js
@@ -0,0 +1,19 @@
+var classPDI_1_1Context__proxy =
+[
+    [ "Context_proxy", "classPDI_1_1Context__proxy.html#a28e4fb8e50fe8b88e01c19082abb1d43", null ],
+    [ "Context_proxy", "classPDI_1_1Context__proxy.html#a3ae61827c42a717817eabbd39be12f78", null ],
+    [ "setup_logger", "classPDI_1_1Context__proxy.html#a2346984133263dfc4b72aa8f45fde8cc", null ],
+    [ "desc", "classPDI_1_1Context__proxy.html#abcdc012d62a65753c918931c38c794c2", null ],
+    [ "desc", "classPDI_1_1Context__proxy.html#ac3d491644cdd6a43ed3744e644fe16a3", null ],
+    [ "operator[]", "classPDI_1_1Context__proxy.html#a24859fa4031c1acc4ad3a0c159377d4e", null ],
+    [ "operator[]", "classPDI_1_1Context__proxy.html#a9608f5160372b9f475172c12351f798d", null ],
+    [ "begin", "classPDI_1_1Context__proxy.html#a2b0e1d5c7a43cd7e594eee845d97ccb1", null ],
+    [ "end", "classPDI_1_1Context__proxy.html#a02f8060d4918a7bb648514a41e190b32", null ],
+    [ "event", "classPDI_1_1Context__proxy.html#a687e53457726aa6a3f3208694b9119b5", null ],
+    [ "logger", "classPDI_1_1Context__proxy.html#a88a323fd7b82649499561d29f578ea0a", null ],
+    [ "pdi_core_logger", "classPDI_1_1Context__proxy.html#a08f57b013269370495575124b3f8a896", null ],
+    [ "datatype", "classPDI_1_1Context__proxy.html#a97ac4737ff55337461e4c3e0b71ee0f0", null ],
+    [ "add_datatype", "classPDI_1_1Context__proxy.html#ab24dac03f6faddd05bd83b734cd1ca56", null ],
+    [ "callbacks", "classPDI_1_1Context__proxy.html#afda9135b7f5fc70799d1390f1887600f", null ],
+    [ "finalize_and_exit", "classPDI_1_1Context__proxy.html#a9c5cec5bf707ad67e38e30a4ee12329b", null ]
+];
---
+++ 1.5/classPDI_1_1Datatype__template.js
@@ -0,0 +1,10 @@
+var classPDI_1_1Datatype__template =
+[
+    [ "Datatype_template", "classPDI_1_1Datatype__template.html#ad2690d41caa281e6f3410dff7caa6c7d", null ],
+    [ "Datatype_template", "classPDI_1_1Datatype__template.html#a4901d777744faf3d9a1ac190ac4706b0", null ],
+    [ "~Datatype_template", "classPDI_1_1Datatype__template.html#a8722c4b873d42931d8e159abaa62ff27", null ],
+    [ "evaluate", "classPDI_1_1Datatype__template.html#a279595fa23744ff10ac01d617e59b8c5", null ],
+    [ "attribute", "classPDI_1_1Datatype__template.html#afa92bd9faeee96f232c694c92be41efe", null ],
+    [ "attributes", "classPDI_1_1Datatype__template.html#a6cd9ace004b0a56baad6efa94146b497", null ],
+    [ "m_attributes", "classPDI_1_1Datatype__template.html#a7cbd797b3fc189c60350f80fb0b43b47", null ]
+];
