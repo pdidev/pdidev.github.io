584d66f18fdba6183582fdf0183d032057af2583
jbigot <jbigot@noreply.example.com>
2023-03-08 17:36:13 +0000

    Update to match b37a6fdf26581a33fca53e8d9af35e78e0a693f0 by repository_dispatch 276

---
+++ 1.6/classPDI_1_1Array__datatype.js
@@ -0,0 +1,23 @@
+var classPDI_1_1Array__datatype =
+[
+    [ "subtype", "classPDI_1_1Array__datatype.html#af94ccd4d0a0de87598b787caaa23ea13", null ],
+    [ "size", "classPDI_1_1Array__datatype.html#a26b0a7a4a180e1135846627372ed9589", null ],
+    [ "start", "classPDI_1_1Array__datatype.html#ac8bc6acc7335a71d63e1a19f2227d2f9", null ],
+    [ "subsize", "classPDI_1_1Array__datatype.html#ab50be2accef49e18e36c9ac96d196a04", null ],
+    [ "densify", "classPDI_1_1Array__datatype.html#a6ce7f8b28320ab5f2ebb34b3e3064e66", null ],
+    [ "evaluate", "classPDI_1_1Array__datatype.html#a7a9ddb0343c43becde0298b68d6312c3", null ],
+    [ "dense", "classPDI_1_1Array__datatype.html#a31286281747dffd22dd10c3a929eef51", null ],
+    [ "datasize", "classPDI_1_1Array__datatype.html#a00fb46532bb0b8a63111130715a10137", null ],
+    [ "buffersize", "classPDI_1_1Array__datatype.html#adb00b9bfc6a70d320111bff1244ad979", null ],
+    [ "alignment", "classPDI_1_1Array__datatype.html#ae744734eedbb48f61e0bde159940d9ce", null ],
+    [ "simple", "classPDI_1_1Array__datatype.html#a9ba8909f75de2d4f18749fe9362a28d2", null ],
+    [ "data_to_dense_copy", "classPDI_1_1Array__datatype.html#aa58ec9cc3cacac4d6aa8e7c7b31323a8", null ],
+    [ "data_from_dense_copy", "classPDI_1_1Array__datatype.html#a9acdd3efed232d9541836269570df05a", null ],
+    [ "index", "classPDI_1_1Array__datatype.html#a976617cebcf2860f0ff78d810f554b23", null ],
+    [ "index", "classPDI_1_1Array__datatype.html#a4fa2b3a5bd604ef0df75ecd1b1fbdccd", null ],
+    [ "slice", "classPDI_1_1Array__datatype.html#aa31e0591ffc5664c613273c7d6989bcb", null ],
+    [ "slice", "classPDI_1_1Array__datatype.html#af834c7006333f788ac3eca46e5914be7", null ],
+    [ "destroy_data", "classPDI_1_1Array__datatype.html#ac4160324349cf4dd13bd36e8894a61cd", null ],
+    [ "debug_string", "classPDI_1_1Array__datatype.html#ae1fa60769b6a03ff0d6b5f0b82523fec", null ],
+    [ "operator==", "classPDI_1_1Array__datatype.html#a0ca6cf15fda5ca1406b24328e5571652", null ]
+];
---
+++ 1.6/classPDI_1_1Data__descriptor.js
@@ -0,0 +1,15 @@
+var classPDI_1_1Data__descriptor =
+[
+    [ "~Data_descriptor", "classPDI_1_1Data__descriptor.html#a7da3ac4a848c78c47f4761b58635610b", null ],
+    [ "default_type", "classPDI_1_1Data__descriptor.html#a8195103097098ffca386df587a394529", null ],
+    [ "default_type", "classPDI_1_1Data__descriptor.html#ae3481358d9a12badc7d015128a37ece4", null ],
+    [ "metadata", "classPDI_1_1Data__descriptor.html#a2ac11dc2986f02d20987fbe65e78ee4b", null ],
+    [ "metadata", "classPDI_1_1Data__descriptor.html#a3fa7ad47543f0ddb94c1481883d7980e", null ],
+    [ "name", "classPDI_1_1Data__descriptor.html#a332663ee9a465c9e5ca3991d70052ac6", null ],
+    [ "ref", "classPDI_1_1Data__descriptor.html#a658635bdd5a35a34024586a297b7909f", null ],
+    [ "empty", "classPDI_1_1Data__descriptor.html#a93b208cdd7f404db906d364ebd834ef2", null ],
+    [ "share", "classPDI_1_1Data__descriptor.html#a76adf3a628997299a690852078ffe9d3", null ],
+    [ "share", "classPDI_1_1Data__descriptor.html#a63a56ba7359038fc621bf2631be6705e", null ],
+    [ "release", "classPDI_1_1Data__descriptor.html#a7a06bfb583e87456264761da9d4caa23", null ],
+    [ "reclaim", "classPDI_1_1Data__descriptor.html#a5d2df7d3fde47975709ae480d76659cf", null ]
+];
