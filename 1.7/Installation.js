10981998874e1c1f023a21f8d8878ebfa8a2a471
jbigot <jbigot@noreply.example.com>
2024-07-21 16:41:45 +0000

    Update to match b37a6fdf26581a33fca53e8d9af35e78e0a693f0 by repository_dispatch 292

---
+++ 1.7/Installation.js
@@ -0,0 +1,12 @@
+var Installation =
+[
+    [ "Installation using binary packages", "Installation.html#installation_using_binary_packages", null ],
+    [ "Installation using Spack recipes", "Installation.html#installation_using_spack_recipes", null ],
+    [ "Installation from source", "Installation.html#installation_from_source", null ],
+    [ "Installation of PDI source distribution", "Source_installation.html", [
+      [ "Downloading PDI source distribution", "Source_installation.html#downloading_distribution", null ],
+      [ "Default installation", "Source_installation.html#default_installation", null ],
+      [ "Configuration", "Source_installation.html#cmake_config", null ],
+      [ "List of dependencies", "Source_installation.html#dependecies", null ]
+    ] ]
+];
